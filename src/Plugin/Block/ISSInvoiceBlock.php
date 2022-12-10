cfd404d5dabc4a9b1c6afaeed7fc69b5162e8b21
lauraRa <ramoslaura418@gmail.com>
2022-12-10 14:37:18 -0600

    add permissions to invoice block

--- src/Plugin/Block/ISSInvoiceBlock.php
+++ src/Plugin/Block/ISSInvoiceBlock.php
@@ -5,6 +5,7 @@ namespace Drupal\iss\Plugin\Block;
 use Drupal\Core\Access\AccessResult;
 use Drupal\Core\Block\BlockBase;
 use Drupal\Core\Session\AccountInterface;
+use Drupal\Core\Url;
 
 /**
  * Provides a invoicing block with all needed fields.
@@ -20,8 +21,34 @@ class ISSInvoiceBlock extends BlockBase {
   * {@inheritdoc}
   */
   public function build() {
-    return \Drupal::formBuilder()->getForm('\Drupal\iss\Form\ISSGetInvoiceDataForm');
+    if (\Drupal::currentUser()->isAuthenticated()) {
+      return \Drupal::formBuilder()->getForm('\Drupal\iss\Form\ISSGetInvoiceDataForm');
+    } else {
+      return [
+        '#markup' => $this->t('Para generar tu factura inicia sesión'),
+      ];
+    }
   }
 
+  /**
+   * {@inheritdoc}
+   */
+  public function blockAccess(AccountInterface $account)
+  {
+    // If viewing a node, get the fully loaded node object.
+    $node = \Drupal::routeMatch()->getParameter('node');
+
+    if (!(is_null($node))) {
+      $request = \Drupal::request();
+      $requestUri = $request->getRequestUri();
+
+      if (strchr($requestUri, '/prueba/exito')) {
+        return AccessResult::allowedIfHasPermission($account, 'view iss block');
+      }
+      
+    }
+
+    return AccessResult::forbidden();
+  }
 
 }
